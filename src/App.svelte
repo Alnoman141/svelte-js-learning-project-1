2521933e45eaf2b3a75cc89224de3328fa318916
Noman <nmnaba14@gmail.com>
2021-04-20 03:10:22 +0600

    project start

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<main>
+	<!-- <Hello on:nameChange={nameChanged} message={message} name={name} greeting={greeting}/> -->
+	<Hello message={message} bind:name={name} greeting={greeting}/>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, temporibus, soluta cumque ratione similique voluptatibus laboriosam tempore voluptate explicabo doloribus nesciunt nisi corporis quae voluptatum labore, eveniet porro dolor nobis autem repellendus suscipit. Nobis molestiae itaque, ipsum quasi delectus suscipit expedita doloribus eveniet deleniti ad, fugiat quos. Doloribus incidunt dolorum quidem perspiciatis ipsam, aliquam, est dolore omnis quibusdam alias fugiat consequuntur consequatur neque aperiam quaerat impedit explicabo. </p>
+	<h2>{name}</h2>
+</main>
+<script>
+	import Hello from "./Hello.svelte";
+	let name = 'Abdullah Al Noman';
+	let greeting = "Hello!";
+	let message = "Your Are Welcome To The Svelte World.";
+
+	// function nameChanged(e){
+	// 	name = e.detail;
+	// }
+</script>
+<style type="text/css">
+	main {
+		width: 1170px;
+		margin: 0 auto;
+		background-color: #ddd;
+		padding: 20px;
+		box-sizing: border-box;
+	}
+
+	
+	main > p {
+		font-size: 30px;
+	}
+</style>
---
+++ src/Hello.svelte
@@ -0,0 +1,27 @@
+<main>
+    <h1>{greeting} {name}. {message}</h1>
+    <!-- <input type="text" bind:value={name} on:keypress={nameChangeEvent} /> -->
+    <input type="text" bind:value={name}  />
+
+</main>
+<script>
+    // import { createEventDispatcher } from 'svelte';
+    // let dispatcher = createEventDispatcher();
+    export let name = 'Shamsia Ferdous Pia';
+	export let greeting;
+    export let message;
+
+    // function nameChangeEvent(e){
+    //     if(e.key === 'Enter'){
+    //         dispatcher('nameChange', name);
+    //     }
+    // }
+</script>
+
+<style>
+    main > h1 {
+		font-size: 50px;
+		text-align: center;
+		font-weight: bolder;
+	}
+</style>
